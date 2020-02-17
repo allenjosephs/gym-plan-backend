f01e455fed5937f69905ed77c2fb47525af49e33
Joe Allen <allenjosephs@johndeere.com>
2020-02-17 17:26:27 -0600

    additional models

--- gym_plan/models.py
+++ gym_plan/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Equipment(models.Model):
     name = models.CharField(max_length=100)
-    img_url = models.TextField(null=True)
-    icon_url = models.TextField(null=True)
+    img_url = models.TextField(null=True, blank=True)
+    icon_url = models.TextField(null=True, blank=True)
 
     class Meta:
         verbose_name = "Equipment"
@@ -13,7 +14,7 @@ class Equipment(models.Model):
         return self.name
 
 class Exercise(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=50)
 
     primary_equip_id = models.OneToOneField(
         Equipment,
@@ -21,6 +22,45 @@ class Exercise(models.Model):
         primary_key=True
     )
 
+    instructions = models.TextField(null=True, blank=True)
+
     def __str__(self):
         return self.name
+
+class Set(models.Model):
+
+    duration = models.PositiveIntegerField(0)
+    repeat_count = models.PositiveIntegerField(0)
+
+    exercises = models.ForeignKey(
+        Exercise,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+class Workout(models.Model):
+        name = models.CharField(max_length=50)
+        description = models.TextField()
+
+        creator_id = models.OneToOneField(
+            User,
+            on_delete=models.PROTECT,
+            primary_key=True
+        )
+
+        users = models.ForeignKey(
+            User,
+            on_delete=models.CASCADE,
+            related_name="shared_with_users"
+        )
+
+        sets = models.ForeignKey(
+            Set,
+            on_delete=models.CASCADE,
+            related_name="sets"
+        )
+
+
 
