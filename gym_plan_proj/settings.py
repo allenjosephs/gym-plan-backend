fef6d15d1b3b96b8d125d7db78f3a2c9ac7c86fd
Joe Allen <allenjosephs@johndeere.com>
2020-02-23 20:49:23 -0600

    debug=True

--- gym_plan_proj/settings.py
+++ gym_plan_proj/settings.py
@@ -28,7 +28,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # Application definition
